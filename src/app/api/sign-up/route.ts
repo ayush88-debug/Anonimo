3a6e29bb7b9ca12832efb356a0e478e3020c3c32
Ayush Ghavghave <ghavghaveayush782@gmail.com>
2025-05-23 14:16:00 +0530

    added sign-up route

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,9 +1,121 @@
+import { sendVerificationMail } from "@/helpers/sendVerificationMail";
 import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from 'bcrypt'
 
 
 export async function POST(request: Request){
 
     dbConnect();
 
-    const {username, email, password}= await request.json();
+    try {
+        const {username, email, password}= await request.json();
+
+        const verifiedUserBYUsername= await UserModel.findOne({
+            username, 
+            isVerified:true
+        })
+
+        if(verifiedUserBYUsername){
+            return Response.json(
+                {
+                    success:false,
+                    message:"Username already exist"
+                },
+                {
+                    status:500
+                }
+            )
+        }
+
+        const userByEmail= await UserModel.findOne({email});
+
+        const verifyCode= Math.floor(100000 + Math.random() * 899999).toString()
+
+        if(userByEmail){
+            if(userByEmail.isVerified){
+                return Response.json(
+                    {
+                        success:false,
+                        message:"Username with Email already exist"
+                    },
+                    {
+                        status:500
+                    }
+                )
+            }else{
+                const hashedPassword= await bcrypt.hash(password, 10);
+                const expiryTime= new Date();
+                expiryTime.setHours(expiryTime.getHours() + 1)
+
+                userByEmail.password= hashedPassword
+                userByEmail.verifycode=verifyCode
+                userByEmail.verifycodeExpiry= expiryTime
+
+                await userByEmail.save()
+            }
+        }else{
+            const hashedPassword= await bcrypt.hash(password, 10);
+            const expiryTime= new Date();
+            expiryTime.setHours(expiryTime.getHours() + 1)
+
+            const newUser= await UserModel.create({
+                    username,
+                    email,
+                    password: hashedPassword,
+                    verifycode: verifyCode,
+                    verifycodeExpiry: expiryTime,
+                    isVerified: false,
+                    isAcceptingMesages: true,
+                    messages: []
+            })
+            await newUser.save()
+        }
+
+        const emailResponse= await sendVerificationMail(
+            email,
+            username,
+            verifyCode
+        );
+
+        if(!emailResponse.success){
+            return Response.json(
+                {
+                    success:false,
+                    message:emailResponse.message
+                },
+                {
+                    status:500
+                }
+            )
+        }
+
+        return Response.json(
+            {
+                success:true,
+                message:"User Registered successfully. Please Verify userself."
+                
+            },
+            {
+                status:201
+            }
+        )
+
+
+        
+    } catch (error) {
+        console.log("Error while user sign up: ",error)
+        return Response.json(
+            {
+                success:false,
+                message:"Error while user sign up"
+            },
+            {
+                status:500
+            }
+        )
+        
+    }
+
+    
 }

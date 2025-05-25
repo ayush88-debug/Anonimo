70262546aa39edfa8ce223a5236b6b30fa120b0c
Ayush Ghavghave <ghavghaveayush782@gmail.com>
2025-05-25 16:56:01 +0530

    fix: add missing semicolon and make user properties optional in next-auth types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,26 +3,26 @@ import { DefaultSession } from "next-auth"
 declare module "next-auth" {
   interface Session {
     user: {
-      _id:string,
-      isVerified: boolean,
-      isAcceptingMesages: boolean,
-      username:string
+      _id?:string,
+      isVerified?: boolean,
+      isAcceptingMesages?: boolean,
+      username?:string
     } & DefaultSession["user"]
   }
   interface User {
-    _id:string,
-    isVerified: boolean,
-    isAcceptingMesages: boolean,
-    username:string
+    _id?:string,
+    isVerified?: boolean,
+    isAcceptingMesages?: boolean,
+    username?:string
  }
 }
 
 
 declare module "next-auth/jwt" {
   interface JWT {
-    _id:string,
-    isVerified: boolean,
-    isAcceptingMesages: boolean,
-    username:string
+    _id?:string,
+    isVerified?: boolean,
+    isAcceptingMesages?: boolean,
+    username?:string
   }
 }

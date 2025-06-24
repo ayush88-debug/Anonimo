c0f8c226d299bf5ec07cf9d524c9cbf67371d2bf
Ayush Ghavghave <ghavghaveayush782@gmail.com>
2025-06-25 05:07:19 +0530

    feat: update nodemailer authentication

--- src/helpers/sendVerificationMail.ts
+++ src/helpers/sendVerificationMail.ts
@@ -8,8 +8,8 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     auth: {
-        user: 'ayush.ghavghave.dev@gmail.com',
-        pass: 'imnedaevrtjeucje'
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASS
     }
 });
 
